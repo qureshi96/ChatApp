a6cfb3234f04f6402d5461424a12df6bc8cc32d7
qureshi96 <usmanramses@gmail.com>
2024-05-21 14:34:36 +0100

    server updated

--- back/server.js
+++ back/server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT ||4000
 
 app.get("/",(req,res) =>{
-    res.send("server running!!");
+    res.send("server is up");
 })
 app.use("/api/auth",authRoutes);
 
